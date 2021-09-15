dda25548f4de59c66a7dc646aaae36698d2e2e31
Luis Peixoto <luispeixoto@pq.uenf.br>
2021-09-15 14:10:02 -0300

    feat: welcome page

---
+++ src/Pages/Welcome/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { StatusBar, Image } from 'react-native'
+import { Container, Title, Vector, Buttons } from './styles'
+import vectorImg from '../../assets/vector.png'
+import Button from '../../components/Button'
+import { useNavigation } from '@react-navigation/core'
+
+const Welcome: React.FunctionComponent = () => {
+  const navigation = useNavigation()
+  return (
+    <Container colors={['#312E37', '#282829']}>
+      <Title>
+        Junte-se a nossa rede e comece a compartilhar as suas ideias
+      </Title>
+      <Vector>
+        <Image source={vectorImg} />
+      </Vector>
+      <Buttons>
+        <Button
+          textColor="#312E37"
+          onPress={() => {
+            navigation.navigate('SignIn')
+          }}
+        >
+          Realizar o login
+        </Button>
+
+        <Button
+          textColor="#E8E8E8"
+          hasBorder={true}
+          onPress={() => {
+            navigation.navigate('SignUp')
+          }}
+        >
+          Cadastra-se
+        </Button>
+      </Buttons>
+    </Container>
+  )
+}
+
+export default Welcome
---
+++ src/Pages/Welcome/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native'
+
+import LinearGradient from 'react-native-linear-gradient'
+
+export const Container = styled(LinearGradient)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 32px;
+`
+export const Title = styled.Text`
+  color: #fff;
+  font-family: 'Lato-Bold';
+  font-size: 32px;
+  line-height: 42px;
+  text-align: center;
+`
+export const Vector = styled.View`
+  margin: 32px 0;
+  align-items: center;
+  justify-content: center;
+`
+export const Buttons = styled.View`
+  flex: 1;
+  width: 100%;
+`
