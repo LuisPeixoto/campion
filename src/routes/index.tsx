75c8f8d5b13a80b55ff4eaac05d105f45968bc24
Luis Peixoto <luispeixoto@pq.uenf.br>
2021-09-15 14:09:11 -0300

    feat: create routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { ActivityIndicator, View } from 'react-native'
+import { useAuth } from '../hooks/auth'
+import AppRoutes from './app.routes'
+import AuthRoutes from './auth.routes'
+
+const Routes: React.FC = () => {
+  const { user, loading } = useAuth()
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          backgroundColor: '#312E37',
+          alignItems: 'center',
+        }}
+      >
+        <ActivityIndicator size="large" color="#fff" />
+      </View>
+    )
+  }
+  return user ? (
+    <>
+      <AuthRoutes />
+    </>
+  ) : (
+    <AppRoutes />
+  )
+}
+
+export default Routes
