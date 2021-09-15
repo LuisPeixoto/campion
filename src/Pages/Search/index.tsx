2ad47f8cd3e3e14043b45a452125cf7a9354b755
Luis Peixoto <luispeixoto@pq.uenf.br>
2021-09-15 14:13:48 -0300

    feat: search page

---
+++ src/Pages/Search/index.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import Icon from 'react-native-vector-icons/Feather'
+import Contact from '../../components/Contact'
+import { Container, Input, TextInput, Content } from './styles'
+
+const Search: React.FunctionComponent = () => {
+  return (
+    <Container colors={['#312E37', '#282829']}>
+      <Content>
+        <Input>
+          <Icon name="search" color="#666263" size={20} />
+          <TextInput
+            placeholder="Faça uma pesquisa"
+            keyboardAppearance="light"
+            placeholderTextColor="#666263"
+          />
+        </Input>
+        <Contact
+          avatar="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          name="Luis Fernando"
+          nickname="luispeixoto"
+        />
+        <Contact
+          avatar="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          name="Luis Fernando"
+          nickname="luispeixoto"
+        />
+        <Contact
+          avatar="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          name="Luis Fernando"
+          nickname="luispeixoto"
+        />
+        <Contact
+          avatar="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          name="Luis Fernando"
+          nickname="luispeixoto"
+        />
+        <Contact
+          avatar="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          name="Luis Fernando"
+          nickname="luispeixoto"
+        />
+        <Contact
+          avatar="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          name="Luis Fernando"
+          nickname="luispeixoto"
+        />
+        <Contact
+          avatar="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          name="Luis Fernando"
+          nickname="luispeixoto"
+        />
+        <Contact
+          avatar="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          name="Luis Fernando"
+          nickname="luispeixoto"
+        />
+        <Contact
+          avatar="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          name="Luis Fernando"
+          nickname="luispeixoto"
+        />
+        <Contact
+          avatar="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          name="Luis Fernando"
+          nickname="luispeixoto"
+        />
+        <Contact
+          avatar="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          name="Luis Fernando"
+          nickname="luispeixoto"
+        />
+      </Content>
+    </Container>
+  )
+}
+
+export default Search
---
+++ src/Pages/Search/styles.ts
@@ -0,0 +1,38 @@
+import LinearGradient from 'react-native-linear-gradient'
+import styled from 'styled-components/native'
+
+export const Container = styled(LinearGradient)`
+  flex: 1;
+`
+
+export const Title = styled.Text`
+  font-size: 28px;
+  font-family: 'Lato-Light';
+  color: #fbf4ee;
+  margin-bottom: 24px;
+`
+
+export const Input = styled.View`
+  width: 100%;
+  height: 60px;
+  padding: 0 16px;
+  background: #23222a;
+  border-radius: 5px;
+  margin-bottom: 8px;
+  flex-direction: row;
+  align-items: center;
+  border-width: 2px;
+  border-color: #23222a;
+`
+export const TextInput = styled.TextInput`
+  flex: 1;
+  color: #666263;
+  font-size: 16px;
+  margin-left: 16px;
+  font-family: 'Talo-Regular';
+`
+
+export const Content = styled.ScrollView`
+  padding: 16px;
+  flex: 1;
+`
