e804fd980caf4e7864c1ddb90cb774fdabfcf931
Luis Peixoto <luispeixoto@pq.uenf.br>
2021-12-04 19:16:57 -0300

    refactor: chat page

--- src/Pages/Chat/styles.ts
+++ src/Pages/Chat/styles.ts
@@ -1,16 +1,36 @@
 import styled from 'styled-components/native'
-import icon from '../../assets/icon.png'
+import { GiftedChat } from 'react-native-gifted-chat'
 
-interface IconProps {
-  icon: string
-}
-
-export const Container = styled.View<IconProps>`
-  width: 45px;
-  height: 45px;
-  align-items: center;
-  justify-content: center;
-
-  border-radius: 40px;
-  background: #e8e8e8;
+export const Container = styled.View`
+  flex: 1;
+  background-color: #312e37;
 `
+export const ChatContent = styled(GiftedChat).attrs({
+  listViewProps: {
+    style: {
+      backgroundColor: '#312E37',
+    },
+  },
+  timeTextStyle: {
+    right: {
+      color: '#000',
+    },
+    left: {
+      color: '#C1C1C1',
+    },
+  },
+  minInputToolbarHeight: 80,
+  placeholderTextColor: '#676266',
+  textInputProps: {
+    style: {
+      backgroundColor: '#23222A',
+      borderRadius: 8,
+      fontSize: 16,
+      marginRight: 15,
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      color: '#C1C1C1',
+      flex: 1,
+    },
+  },
+})``
